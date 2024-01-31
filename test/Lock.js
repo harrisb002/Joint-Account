0b53877920a83f288542e9b3626d672b2eaa832b
harrisb002 <harrisbe@sonoma.edu>
2024-01-31 01:30:35 -0800

    test deploying contract

--- test/Lock.js
+++ test/Lock.js
@@ -1,17 +1,34 @@
+//loadFixture: Takes another function it can call so we can use the cached state of the contract
+//Results in not having to redeploy the contract for every test case
 const {
   loadFixture,
 } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 
-describe("Lock", function () {
-  async function deployOneYearLockFixture() {
+describe("BankAccount", function () {
+  async function deployBankAccount() {
+
     // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await ethers.getSigners();
+    // Can get the different accounts to use to sign tx's
+    const [address0, address1, address3, address4] = await ethers.getSigners(); 
 
-    const Lock = await ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy();
+    //Automatically gets BankAccount from the contract directory defined in folder
+    const BankAccount = await ethers.getContractFactory("BankAccount");
 
-    return { lock, owner, otherAccount };
+    //Once deployed, return the bankAccount instance
+    const bankAccount = await BankAccount.deploy();
+
+    //All the address that can be used to connect to instance and smart contract
+    return { bankAccount, address0, address1, address3, address4};
   }
+
+  //Block tests using the subheaders through describes
+
+  //Make sure the contract can deploy successfully
+  describe("Deployment", () => {
+    it("Should deploy without error", async () => {
+      await loadFixture(deployBankAccount) //Will call if not called before or
+    })
+  })
 });
